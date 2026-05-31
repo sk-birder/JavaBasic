09effe5405a4ebc889dad2d8383bbfe98d33362b
sk-birder <kibe.shuhei@gmail.com>
2026-05-31 23:41:20 +0900

    Section05

---
+++ 05/Ex0501.java
@@ -0,0 +1,12 @@
+class Ex0501 {
+  public static void main(String[] args) {
+    int price = 300 ;
+    int point = 9 ;
+    System.out.println("金額:" + price + "円") ;
+    System.out.print("獲得したポイント:" + point + "ポイント") ;
+  }
+}
+
+/*
+  変数名を一括で変えるときはF2で変えることが出来る(今回はamont->priceに変更)
+*/
---
+++ 05/Ex0502.java
@@ -0,0 +1,9 @@
+class Ex0502 {
+  public static void main(String[] args) {
+    final int START_TIME = 9 ;  // 定数はSNAKE_CASE
+    int currentTime =  7 ;      // 変数はlowerCamelCase
+
+    System.out.println("開始時刻:" + START_TIME + "時") ;
+    System.out.print("現在時刻:" + currentTime + "時") ;
+  }
+}
